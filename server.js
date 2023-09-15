ac82d8b0a1a52cc76ce2ef3c334a517ac063f780
timotakeuchi <117367638+timotakeuchi@users.noreply.github.com>
2023-09-15 10:07:14 -0400

    Indented code
    Properly indented code for neatness

--- server.js
+++ server.js
@@ -1,32 +1,40 @@
 const http = require('http'),
-      fs = require('fs'),
-      url = require('url');
+  fs = require('fs'),
+  url = require('url');
 
 http
   .createServer((request, response) => {
     let addr = request.url,
-        p = url.parse(addr, true),
-        filePath= '';
+      p = url.parse(addr, true),
+      filePath = '';
 
-    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp:' 
-  + new Date() + '\n\n', (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Added to log.');
+    fs.appendFile(
+      'log.txt',
+      'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n',
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Added to log.');
+        }
       }
-    });    
+    );
 
     if (p.pathname.includes('documentation')) {
-      filePath = (__dirname + '/documentation.html');
-     } else {
+      filePath = __dirname + '/documentation.html';
+    } else {
       filePath = 'index.html';
-     }
     }
 
-    response.writeHead(200, { 'Contnt-Type': 'text/plain' });
-    response.end('Hello Node!\n');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      response.writeHead(200, { 'Contnt-Type': 'text/html' });
+      response.write(data);
+      response.end('Hello Node!\n');
+    });
   })
   .listen(8080);
-
 console.log('My first Node test server is running on Port 8080.');
